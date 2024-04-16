6ca46a27f53b46466baad9877f5feee1c035b3f0
Christian Kuechler <ckuechler@gmail.com>
2024-04-16 16:58:08 +0200

    Day two tasks done

--- src/quiz.js
+++ src/quiz.js
@@ -1,44 +1,61 @@
 class Quiz {
-    // YOUR CODE HERE:
-
-    constructor (questions, timeLimit, timeRemaining) {
+    constructor(questions, timeLimit, timeRemaining) {
         this.questions = questions;
         this.timeLimit = timeLimit;
         this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0; 
+        this.correctAnswers = 0;
         this.currentQuestionIndex = 0;
     }
 
     getQuestion() {
-    return this.questions[this.currentQuestionIndex]    
-    }    
+        return this.questions[this.currentQuestionIndex]
+    }
     moveToNextQuestion() {
         this.currentQuestionIndex++;
     }
 
     shuffleQuestions() {
-       let copy = [...this.questions];
-       let newArray = [];
-       let originalLength = copy.length
-       while (newArray.length < originalLength) {
-           const randomR = Math.floor(Math.random() * copy.length )
-           newArray.push(copy[randomR])
-           copy.splice(randomR,1)
-       }
-       this.questions = newArray;
+        let copy = [...this.questions];
+        let newArray = [];
+        let originalLength = copy.length
+        while (newArray.length < originalLength) {
+            const randomR = Math.floor(Math.random() * copy.length)
+            newArray.push(copy[randomR])
+            copy.splice(randomR, 1)
+        }
+        this.questions = newArray;
     }
 
     checkAnswer(answer) {
-        if (answer === this.questions[this.currentQuestionIndex].answer ) {
+        if (answer === this.questions[this.currentQuestionIndex].answer) {
             this.correctAnswers++;
         }
     }
 
     hasEnded() {
         if (this.currentQuestionIndex < this.questions.length) {
-        return false
+            return false
         }
         return true
     }
+
+    filterQuestionsByDifficulty(difficulty) {
+        if (difficulty >= 1 && difficulty <= 3) {
+            let arr = this.questions.filter((element) => {
+                return element.difficulty === difficulty;
+            });
+            this.questions = arr;
+        }
+
+    }
+
+    averageDifficulty() {
+     
+        const avg = this.questions.reduce(function (accumulator, element) {
+            return accumulator += element.difficulty;
+        },0);
+        return avg / this.questions.length;
+    }
+
 }
 
